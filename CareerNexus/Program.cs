ef0881f1a4ea0a40b370b4b757d99ae30aed4ce5
Adeel <ma7088450@gmail.com>
2025-10-04 02:15:40 -0700

    CS-03 add Cors in program.cs

--- CareerNexus/Program.cs
+++ CareerNexus/Program.cs
@@ -94,6 +94,15 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:8080")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
